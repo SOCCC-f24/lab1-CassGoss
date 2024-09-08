ba18b996c4b0316165c32065053d14e00e07f594
CassGoss <cg159@sunyorange.edu>
2024-09-08 14:02:47 -0400

    Update temp_converter.py
    new changes

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
-
+"""
+temp.py - converts Celsius temperatures to Fahrenheit.
+Name:    Cassidy Goss
+Date:    20240905
+Course Number:    138
+Class Section:    EN
+Changes:
+    20240908 - Added Parenthesis, and comments.
+    """
 # process
 def c2f(c):
-    return c * 9 / 5 + 32
+    return (c * 9 / 5) + 32
 
 def main(cel):
     return c2f(cel)
-
+    
 if __name__ == "__main__":
-    cel = 100         # input
+    cel =  100        # input
     print(main(cel))  # output
