11bff16abf18122dab1cb30f7c7b2e6c09d2fb27
CassGoss <cg159@sunyorange.edu>
2024-09-14 10:33:42 -0400

    Update temp_converter.py

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -5,8 +5,6 @@ Name:    Cassidy Goss
 Date:    20240905
 Course Number:    138
 Class Section:    EN
-Changes:
-    20240908 - Added Parenthesis, and comments.
     """
 # process
 #def c2f(c):
@@ -16,9 +14,9 @@ def main():
     f = 0
     c = f2c(f)
     print(f"{f}F is {c} C")
-if__name__ == "__main__":
-        main()
-    
+if __name__ == “__main__”:
+	main()
+
 def f2c_raw(f): #IGNORE ME
 	return f - 32 * 5 / 9
 def f2c_op(f): 
