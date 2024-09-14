eef4b0654ba2bf9c8430a0e797c40f3ac10250c7
CassGoss <cg159@sunyorange.edu>
2024-09-14 11:13:10 -0400

    Update temp_converter.py
    Final Update

--- temp_converter/temp_converter.py
+++ temp_converter/temp_converter.py
@@ -5,19 +5,19 @@ Name:    Cassidy Goss
 Date:    20240905
 Course Number:    138
 Class Section:    EN
-    """
+"""
 # process
 #def c2f(c):
-#    return c * 9 / 5 + 32
+#    return (c * 9 / 5) + 32 #Celsius is inputted and Farenheit is the output.
 def f2c_raw(f):
-    return f - 32 * 5 / 9
+    return (f - 32 * 5) / 9 #Farenheit is the input, and Celsius is the output. 
 
 def f2c_op(f):
-    return (f - 32) * 5 / 9
+    return (f - 32 * 5) / 9
 
 def main():
-    f = 0           # input
-    c = f2c(f)
+    f = 0           # input, setting the base value for Farenheit.
+    c = f2c(f)      # Celsius is equal to the conversion of Farenheit to Celsius.
     print(f"{f}F is {c} C")
 
 if __name__ == "__main__":
